927d8eded94e150deaee9e9698499eb7673a5c54
dani nawaz <raheem.dev7@gmail.com>
2025-01-30 00:23:34 +0500

    animation fix

--- sections/HowItWorks.jsx
+++ sections/HowItWorks.jsx
@@ -146,7 +146,6 @@ export default function HowItWorks() {
                 How it works?
             </motion.h1>
             <FirstStep onProposalClick={handleProposalClick}/>
-            <FourthStep/>
             {currentStep >= 1 && (
                 <>
                     <SecondStep onEvaluatorListClick={handleEvaluatorListClick}/>
@@ -221,18 +220,50 @@ function SecondStep({onEvaluatorListClick}) {
             <h2 className="text-2xl">2. Select Evaluators List</h2>
             <p className="text-lg">Select any evaluators from the list below to get started.</p>
             <div className="flex gap-4">
-                {mailingLists.map((mailingList) => (
-                    <motion.div
-                        key={mailingList.id}
-                        className="flex flex-col gap-2 bg-gray-50 hover:bg-white hover:border-gray-500 p-4 rounded cursor-pointer"
-                        onClick={onEvaluatorListClick}
-                        whileHover={{scale: 1.05}}
-                        whileTap={{scale: 0.95}}
-                    >
-                        <h3 className="text-lg font-bold">{mailingList.title}</h3>
-                        <p className="text-sm">{mailingList.description}</p>
-                    </motion.div>
-                ))}
+                          {discussions.map((discussion) => (
+                            <motion.div
+                                    className="flex flex-col gap-2 bg-gray-50 hover:bg-white hover:border-gray-500 p-4 rounded cursor-pointer"
+                                    onClick={onEvaluatorListClick}
+                                    whileHover={{scale: 1.05}}
+                                    whileTap={{scale: 0.95}}
+                                    key={discussion.id}
+                            >
+                              <div>
+                                <p className="text-sm/6 font-semibold text-gray-900">
+                                  <a href={discussion.href} className="hover:underline">
+                                    {discussion.title}
+                                  </a>
+                                </p>
+                                <div className="mt-1 flex items-center gap-x-2 text-xs/5 text-gray-500">
+                                  <p>
+                                    <a href={discussion.author.href} className="hover:underline">
+                                      {discussion.author.name}
+                                    </a>
+                                  </p>
+                                  <svg viewBox="0 0 2 2" className="size-0.5 fill-current">
+                                    <circle r={1} cx={1} cy={1} />
+                                  </svg>
+                                  <p>
+                                    <time dateTime={discussion.dateTime}>{discussion.date}</time>
+                                  </p>
+                                </div>
+                              </div>
+                              <dl className="flex w-full flex-none justify-between gap-x-8 sm:w-auto">
+                                <div className="flex -space-x-0.5">
+                                  <dt className="sr-only">Commenters</dt>
+                                  {discussion.commenters.map((commenter) => (
+                                    <dd key={commenter.id}>
+                                      <img
+                                        alt={commenter.name}
+                                        src={commenter.imageUrl}
+                                        className="size-6 rounded-full bg-gray-50 ring-2 ring-white"
+                                      />
+                                    </dd>
+                                  ))}
+                                </div>
+                              </dl>
+                            </motion.div>
+                          ))}
             </div>
         </motion.div>
     )
@@ -362,3 +393,120 @@ function FourthStep() {
     );
 }
 
+
+const discussions = [
+  {
+    id: 1,
+    title: 'Set 1',
+    href: '#',
+    author: { name: 'Leslie Alexander', href: '#' },
+    date: '2d ago',
+    dateTime: '2023-01-23T22:34Z',
+    status: 'active',
+    totalComments: 24,
+    commenters: [
+      {
+        id: 12,
+        name: 'Emma Dorsey',
+        imageUrl:
+          'https://images.unsplash.com/photo-1505840717430-882ce147ef2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+      {
+        id: 6,
+        name: 'Tom Cook',
+        imageUrl:
+          'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+      {
+        id: 4,
+        name: 'Lindsay Walton',
+        imageUrl:
+          'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+      {
+        id: 16,
+        name: 'Benjamin Russel',
+        imageUrl:
+          'https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+      {
+        id: 23,
+        name: 'Hector Gibbons',
+        imageUrl:
+          'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: 'Set 2',
+    href: '#',
+    author: { name: 'Dries Vincent', href: '#' },
+    date: '3d ago',
+    dateTime: '2023-01-22T12:59Z',
+    status: 'resolved',
+    totalComments: 22,
+    commenters: [
+      {
+        id: 19,
+        name: 'Lawrence Hunter',
+        imageUrl:
+          'https://images.unsplash.com/photo-1513910367299-bce8d8a0ebf6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+      {
+        id: 21,
+        name: 'Angela Fisher',
+        imageUrl:
+          'https://images.unsplash.com/photo-1501031170107-cfd33f0cbdcc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+      {
+        id: 14,
+        name: 'Jenny Wilson',
+        imageUrl:
+          'https://images.unsplash.com/photo-1507101105822-7472b28e22ac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+      {
+        id: 16,
+        name: 'Benjamin Russel',
+        imageUrl:
+          'https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: 'Set 3',
+    href: '#',
+    author: { name: 'Courtney Henry', href: '#' },
+    date: '5d ago',
+    dateTime: '2023-01-20T20:12Z',
+    status: 'active',
+    totalComments: 15,
+    commenters: [
+      {
+        id: 11,
+        name: 'Kristin Watson',
+        imageUrl:
+          'https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+      {
+        id: 6,
+        name: 'Tom Cook',
+        imageUrl:
+          'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+      {
+        id: 10,
+        name: 'Emily Selman',
+        imageUrl:
+          'https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+      {
+        id: 16,
+        name: 'Benjamin Russel',
+        imageUrl:
+          'https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+    ],
+  },
+]
