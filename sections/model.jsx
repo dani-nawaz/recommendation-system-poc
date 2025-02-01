2f273e885400fcd030d9b6581c9d61180bddc5b2
dani nawaz <raheem.dev7@gmail.com>
2025-02-02 01:16:56 +0500

    padding

--- sections/model.jsx
+++ sections/model.jsx
@@ -30,7 +30,7 @@ export default function Model() {
     ];
 
     return (
-        <article className="flex flex-col gap-6  justify-center overflow-hidden  sm:pb-0 h-screen">
+        <article className="flex flex-col gap-6 justify-center overflow-hidden pb-12 sm:pb-0 h-dvh">
             <div className="flex flex-col gap-2 sm:gap-4">
                 <h1>
                     Solution
