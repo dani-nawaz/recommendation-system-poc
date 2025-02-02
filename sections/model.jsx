2f96911b20fb7c2cb3a6d334e47f5b4b807f5de4
dani nawaz <raheem.dev7@gmail.com>
2025-02-02 12:44:21 +0500

    changes

--- sections/model.jsx
+++ sections/model.jsx
@@ -49,7 +49,8 @@ export default function Model() {
             </div>
 
             <div className='flex justify-start gap-4'>
-                <a href="https://www.linkedin.com/company/clickchain/" className="bg-cyan-600 text-base sm:text-xl text-gray-50 p-4 flex gap-4 rounded w-max cursor-pointer items-center">Find us</a>
+                <a href="https://www.linkedin.com/company/clickchain/"   target="_blank"
+  rel="noopener noreferrer" className="bg-cyan-600 text-base sm:text-xl text-gray-50 p-4 flex gap-4 rounded w-max cursor-pointer items-center">Find us</a>
                 <a href="https://clickchain.com/" className="bg-gray-300 text-base sm:text-xl text-gray-70 p-4 flex gap-4 rounded w-max cursor-pointer items-center">Back to clickchain</a>
             </div>
 
